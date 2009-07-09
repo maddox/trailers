7a8c937085190b1edfdc91193910b38db450e720
Mike Schrag <mschrag@pobox.com>
2009-07-09 00:05:13 -0400

    apple trailers pull properly for the iphone

--- trailers/apple_trailer_source.rb
+++ trailers/apple_trailer_source.rb
@@ -1,16 +1,33 @@
 class AppleTrailerSource < TrailerSource
   def initialize
     @trailers = {}
-    Nokogiri::XML(open('http://www.apple.com/trailers/home/xml/current.xml')).xpath('//records/movieinfo/info').each do |movie|
+    JSON.parse(open('http://www.apple.com/trailers/iphone/home/feeds/studios.json').read).each do |movie|
       trailer = Trailer.new
-      trailer.name = movie.at('title').content
-      trailer.url = movie.xpath('//preview/large').first.content
+      trailer.name = movie['title']
+      trailer.processed = false
+      trailer.url = "http://www.apple.com#{movie['trailers'][0]['url']}"
       trailer.source = 'apple'
       @trailers[trailer.name.downcase] = trailer
     end
   end
 
   def find_by_name(name)
-    return @trailers[name.downcase]
+    trailer = @trailers[name.downcase]
+    if (!trailer.processed)
+      redirect_doc = open(trailer.url).read
+      if (redirect_doc =~ /<meta http-equiv="refresh" content="1; URL=([^"]+)">/)
+        trailer_doc = Hpricot(open("http://www.apple.com#{$1}"))
+      else
+        trailer_doc = Hpricot(redirect_doc)
+      end
+      trailer_embed = (trailer_doc/"embed").first
+      if (trailer_embed)
+        trailer.url = trailer_embed['href']
+        trailer.processed = true
+      else
+        trailer = nil
+      end
+    end
+    trailer
   end
 end
